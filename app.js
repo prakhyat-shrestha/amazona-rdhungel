3eaf2970f324b6bc8cd43426cbd86ac7648bc1e6
prakhyat-shrestha <erprakhyat@gmail.com>
2021-06-14 08:04:02 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB connected"));
+
+app.get("/", (req, res) => {
+  res.send("hello from node");
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
